d961eba787bef98fef735dfa2e44c7ed0ce9754c
Noa Simon <noasimon88@gmail.com>
2024-06-02 13:12:57 +0300

    fix business size

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ function exportToPdf() {
     html2canvas(businessCard).then(function(canvas) {
         // Add the screenshot to the PDF
         var imgData = canvas.toDataURL('image/png');
-        pdf.addImage(imgData, 'PNG', 0, 0);
+        
+        // Calculate the scale factor
+        var scaleFactor = 1;
+        var desiredWidth = 326;  // The width of the business card in your CSS
+        var actualWidth = canvas.width;
+        scaleFactor = desiredWidth / actualWidth;
+
+        // Add the screenshot to the PDF with scaling
+        pdf.addImage(imgData, 'PNG', 0, 0, actualWidth * scaleFactor, canvas.height * scaleFactor);
+        // pdf.addImage(imgData, 'PNG', 0, 0);
         
         // Save the PDF
         pdf.save('BusinessCard.pdf');
